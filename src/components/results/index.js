7c464cdf443950303eddd1d979bc5c9544683987
nurselaine <Elaine.t.huynh@gmail.com>
2022-10-24 23:46:51 +0000

    refactored class components to functional components

---
+++ src/components/results/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Results(props) {
+    return (
+      <section>
+        <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+      </section>
+    );
+}
